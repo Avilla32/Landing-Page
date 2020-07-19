21bce025e79db160d63a39889a5c8de3556a5491
Alex <alex.m.villalpando@gmail.com>
2020-07-19 19:34:32 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ createNavBar();
 function isInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= -400 &&
+        rect.top >= 0 &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
