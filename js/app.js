0f8516626ea9bc52adc8175488c77b8f74430803
Alex <alex.m.villalpando@gmail.com>
2020-07-19 14:16:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function getSectionNames() {
     let i = 0;
     const sectionNames = document.querySelectorAll('section');
     for (i = 0; i < sectionNames.length; i++) {
-       let sectionName = sectionNames[i].getAttribute('id');
+       const sectionName[] = sectionNames[i].getAttribute('id');
         console.log(sectionName);
     }
 }
@@ -16,10 +16,8 @@ function getSectionNames() {
 // calls the function to get sections on page
 //getSectionNames()
 
-
 /*
-const navItem = document.createElement("LI");                 // Create a <li> node
-const navText = document.createTextNode(getSectionNames([]));         // Create a text node
+let navItem = document.createElement("LI");                 // Create a <li> node
+let navText = document.createTextNode(getSectionNames([]));         // Create a text node
 navItem.appendChild(navText);                              // Append the text to <li>
 document.getElementById("navbar__list").appendChild(navItem);     // Append <li> to <ul> with id="navbar__list" 
-*/ 
