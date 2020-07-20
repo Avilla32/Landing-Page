7a29336644de101c0779212386f793adb3f432f2
Alex <alex.m.villalpando@gmail.com>
2020-07-20 10:18:46 +0200

    removed i=0

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // gets name of sections on page
 function getSectionNames() {
-    let i = 0;
     let sectionName = [];
     const sectionNames = document.querySelectorAll('section');
     for (i = 0; i < sectionNames.length; i++) {
@@ -18,7 +17,6 @@ function getSections() {
 //creates navBar
 
 function createNavBar() {
-    let i = 0;
     const navItems = getSections();
 
     for (i = 0; i < navItems.length; i++) {
