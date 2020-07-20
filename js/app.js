3a5c642b8c3d4605e8643ba65f51f620ed8038cc
Alex <alex.m.villalpando@gmail.com>
2020-07-20 09:45:54 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -10,11 +10,15 @@ function getSectionNames() {
     return sectionName
 }
 
+function getSections() {
+    const sections = document.querySelectorAll('section');
+}
+
 //creates navBar
 
 function createNavBar() {
     let i = 0;
-    const navItems = document.querySelectorAll('section');
+    const navItems = getSections();
 
     for (i = 0; i < navItems.length; i++) {
         let items = getSectionNames();
@@ -32,6 +36,7 @@ function createNavBar() {
 createNavBar();
 
 
+
 // sees which section is in view and activates class
 
 function isInViewport(el) {
@@ -45,24 +50,27 @@ function isInViewport(el) {
     );
 }
 
-let i = 0;
-const sections = document.querySelectorAll('section');
-for (i = 0; i < sections.length; i++) {
-    let section = getSectionNames();
-    const box = document.querySelector('#' + section[i]);
-    const navSelector = document.querySelector('#' + 'nav' + section[i]);
+function activeState() {
+    const sections = getSections();
     document.addEventListener('scroll', function () {
+            for (i = 0; i < sections.length; i++) {
+                let section = getSectionNames();
+                const box = document.querySelector('#' + section[i]);
+                const navSelector = document.querySelector('#' + 'nav' + section[i]);
 
-        if (isInViewport(box)) {
-            box.classList.add('your-active-class');
-            navSelector.classList.add('nav-active');
 
-        } else {
-            box.classList.remove('your-active-class');
-            navSelector.classList.remove('nav-active');
-        }
+                if (isInViewport(box)) {
+                    box.classList.add('your-active-class');
+                    navSelector.classList.add('nav-active');
 
-    }, {
-        passive: true
-    });
+                } else {
+                    box.classList.remove('your-active-class');
+                    navSelector.classList.remove('nav-active');
+                }
+
+            }, {
+                passive: true
+            });
+    }
 }
+activeState();
